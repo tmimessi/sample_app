249c5cf921649876c3916e16a6d201841bab028c
Tainá Mimessi <taina.mimessi@hotmail.com>
2024-06-13 15:31:50 -0300

    add css

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  # This rule maps requests for the URL /static_pages/home to the home action in the Static Pages controller. By using GET we arrange for the route to respond to a GET request.
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get "/help", to: 'static_pages#help'
+  get "/about", to: 'static_pages#about'
+  get "/contact", to: 'static_pages#contact'
 end
