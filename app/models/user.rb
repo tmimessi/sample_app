3109603a23e7885436ce66dc0a25080af7907512
Tainá Mimessi <taina.mimessi@hotmail.com>
2024-06-27 12:17:48 -0300

    chore: Add password reset functionality and related views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -55,6 +55,22 @@ class User < ApplicationRecord
     # sends activation email
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+
+    # sets the password reset attributes
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    end
+
+    # sends password reset email
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    # returns true if a password reset has expired
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     private
