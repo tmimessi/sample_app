b298dd541291cf7ab5da1d9549ad110b3da80409
Tainá Mimessi <taina.mimessi@hotmail.com>
2024-07-03 11:52:15 -0300

    refactor: Optimize user feed query for improved performance

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -107,6 +107,7 @@ class UserTest < ActiveSupport::TestCase
     # self-posts for user with followers
     michael.microposts.each do |post_self| 
       assert michael.feed.include?(post_self)
+      assert_equal michael.feed.distinct, michael.feed
     end
     # posts from non-followed user
     archer.microposts.each do |post_unfollowed|
